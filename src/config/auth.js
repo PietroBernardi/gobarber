4da819fffd435251e2fedf4e5037bdda4e8db697
PietroBernardi <pietro.henrique.bernardi@gmail.com>
2020-02-11 20:04:24 -0300

    sessions

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'eeae64b14604ea6e39e995dc749102a8',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,17 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Pietro Bernardi',
-    email: 'pietrobernardi@com.br',
-    password_hash: '123456789',
-  });
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
+routes.use(authMiddleware);
 
-  return res.json(user);
-});
+routes.put('/users', UserController.update);
 
 export default routes;
