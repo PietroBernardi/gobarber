e4e3c36ff9541c7017aa52d256c5f925c000fbbc
PietroBernardi <pietro.henrique.bernardi@gmail.com>
2020-02-17 20:52:05 -0300

    mail 1

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '31c7f03b77e777',
+    pass: 'e51d5eaa428cd8'
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarbr.com>'
+  }
+};
